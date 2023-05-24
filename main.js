f6a2c86ae8b98a1e61023d3e3edcfa563964834d
BhavikaBangtan <121876684+BhavikaBangtan@users.noreply.github.com>
2023-05-24 14:15:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-//https://teachablemachine.withgoogle.com/models/94HZyzPlx/
+
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    animal=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/94HZyzPlx/,Anime');
+}
+function dance(){
+    animal.classify(result)
+}
+
